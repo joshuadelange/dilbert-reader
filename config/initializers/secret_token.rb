932f0199c90b8551aada95acad69d8fd8a9c26c9
Joshua de Lange <joshua.delange@gmail.com>
2013-09-06 14:17:06 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DilbertReader::Application.config.secret_token = '2807c2ff6e477751dd5266c3e969e6bbbc20532da1d0599341f287454a84af0aaca2429ade399e8e54905326c07364cff5a1b8d6371cf8f2e9831c3f934171d4'
